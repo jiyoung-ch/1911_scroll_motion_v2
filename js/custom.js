1407f3d5a16d6bec4c80b09f50f5fd6561a05b54
jy.choi <chchch007008@gmail.com>
2019-11-25 17:42:10 +0900

    수정

--- js/custom.js
+++ js/custom.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
             $("#wrap").find(".section1").find(".title_animate").find("span:even").stop().animate({
                 now: 0
             }, {
-                duration: 1000,
+                duration: 500,
                 step: function (now) {
                     $(this).css({
                         transform: 'translateY(' + now + '%)'
@@ -49,7 +49,7 @@ $(document).ready(function () {
             $("#wrap").find(".section1").find(".title_animate").find("span:even").stop().animate({
                 now: 50
             }, {
-                duration: 1000,
+                duration: 500,
                 step: function (now) {
                     $(this).css({
                         transform: 'translateY(' + now + '%)'
@@ -63,10 +63,10 @@ $(document).ready(function () {
                 "transform": "translate(0, 0)",
                 "opacity": 1
             });
-            $(".section2").find(".img_bg").addClass("on");
-            $(".section2").find(".img_bg").css({
-                "opacity": 1
-            });
+            $(".section2").find(".img_bg").addClass("on")
+            // $(".section2").find(".img_bg").css({
+            //     "opacity": 1
+            // });
         }
 
         if (scroll >= pos2 + 200) {
@@ -80,27 +80,38 @@ $(document).ready(function () {
         //슬라이드
         var cnt = 0;
         var preCnt = 0;
-        var length = $(".img_group").find(".img_bg").length;
+        var imgBg = $(".img_group").find(".img_bg");
+        var imgKeynote = $(".img_group").find(".keynote");
+        var length = imgBg.length;
+        var state = true;
 
         $(".btn_next").on("click",function(e){
             e.preventDefault();
 
             preCnt = cnt;
             cnt++;
-            console.log(cnt);
 
-            if(cnt >= length){
-                cnt = 0;
-            };
+            if(state === true){
+                state = false;
+                console.log(state);
 
-            $(".img_group").find(".img_bg").eq(preCnt).stop().animate({left:"-100%"},500,function(){
-                $(this).css("left","100%");
-            });
+                if(cnt >= length){
+                    cnt = 0;
+                };
+                console.log(cnt);
 
-            $(".img_group").find(".img_bg").eq(cnt).stop().animate({left:"0"},500).addClass("on").siblings().removeClass("on");
+                imgBg.eq(preCnt).stop().animate({left:"-100%"},500,function(){
+                    $(this).css("left","100%");
+                });
 
-            $(".img_group").find(".keynote").removeClass("on");
-            $(".img_group").find(".keynote").eq(cnt).addClass("on").siblings().removeClass("on");
+                imgBg.eq(cnt).stop().animate({left:"0"},500).addClass("on").siblings().removeClass("on");
+
+                imgKeynote.removeClass("on");
+                imgKeynote.eq(cnt).addClass("on").siblings().removeClass("on");
+            }
+
+            state = true;
+
         });
 
         $(".btn_prev").on("click",function(e){
@@ -109,13 +120,22 @@ $(document).ready(function () {
             preCnt = cnt;
             cnt--;
 
-            if(cnt < 0){
-                cnt = length-1;
+            if(state === true){
+                state = false;
+
+                if(cnt < 0){
+                    cnt = length-1;
+                };
+
+                imgBg.eq(cnt).css("left",'-100%').stop().animate({left:"0"},500).addClass("on").siblings().removeClass("on");
+                imgKeynote.removeClass("on");
+                imgKeynote.eq(cnt).addClass("on").siblings().removeClass("on");
+
+                imgBg.eq(preCnt).stop().animate({left:"100%"},500);
+
+                state = true;
             }
 
-            $(".img_group").find(".img_bg").eq(cnt).css("left",'-100%').stop().animate({left:"0"},500).addClass("on").siblings().removeClass("on");
-
-            $(".img_group").find(".img_bg").eq(preCnt).stop().animate({left:"100%"},500);
         });
 
 
